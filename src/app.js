9306f3fbeb538a3d3cb26404361ea25378773d37
Davy-Santos <davy.santos06@gmail.com>
2025-09-23 16:15:09 -0300

    Simulando problemas

--- src/app.js
+++ src/app.js
@@ -19,4 +19,5 @@ app.get('/', (req, res) => {
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log("⚠️ Debug: servidor iniciado"); // simula um problema
 });
